163784e3828185bcfdbf57b3dcb9c837c0a5fae2
ryanorsinger <ryan.orsinger@gmail.com>
2015-02-27 12:21:40 -0600

    Add simon in vanilla js

--- js/simon.js
+++ js/simon.js
@@ -1,47 +1,128 @@
-//Declaring Strict Mode
-"use strict";
-
-var simonSequence = [];
 var userSequence = [];
+var simonSequence = [];
 
 var squares = document.getElementsByClassName('square');
 
-function addRandomSquareToSequence() {
-    var random = Math.floor(Math.random() * 4);
-    simonSequence.push(squares[random]);
-    return squares[random];
+function start() {
+    simonSequence = [];
+    next();
 }
 
-function animate(element) {
-    element.style.opacity = "1";
-    var fadeoutTimerId = setTimeout(function() {
-        element.style.opacity = "0.3";
+function next() {
+    userSequence = [];
+    addRandomSquareToSequence();
+    playback();
+}
+
+function addRandomSquareToSequence() {
+    var random = Math.floor(Math.random() * 4);
+    simonSequence.push(squares[random].id);
+}
+
+function playback() {
+    // disable user input
+    disableInput();
+
+    // update score
+    document.getElementById('round').innerText = "Round: " + simonSequence.length;
+
+    // create the counter variable
+    var i = 0;
+
+    // do the following at an interval based on the duration
+    var intervalId = setInterval(function() {
+
+        lightUp(document.getElementById(simonSequence[i]));
+
+        i++;
+
+        // check to see if the counter is equal to the game sequence length
+        if (i >= simonSequence.length) {
+            // stop repeating this interval
+            clearInterval(intervalId);
+
+            // call enableInput() function
+            enableInput();
+        }
     }, 1000);
 }
 
-function userClick() {
-    console.log(this);
-    animate(this);
+function lightUp(element) {
+    element.style.opacity = "1";
+    var fadeoutTimerId = setTimeout(function() {
+        element.style.opacity = "0.3";
+    }, 600);
 }
 
-function simon(intervalId) {
-    for(var i=0; i <= simonSequence.length; i++) {
-        if (i == simonSequence.length) {
-            clearInterval(intervalId);
-        } else {
-            console.log(simonSequence[i]);
-            addRandomSquareToSequence();
-        }
+function compareSequences() {
+    var sequenceError = false;
+
+    // loop through the input sequence and perform the following:
+    // check it the game sequence and input sequence match for a given offset
+    // if they don't match set your error status variable to true and get out of the loop
+    for (var i = 0; i < userSequence.length; i++) {
+      if (simonSequence[i] == undefined || simonSequence[i] != userSequence[i]) {
+        sequenceError = true;
+        break;
+      }
+    }
+
+    // check if there is an error
+    // if there is, call gameOver()
+    // otherwise, check if the input sequence is the same length as the game sequence
+    // if it is, the user has completed the sequence, so clear the input sequence and call next()
+    if (sequenceError) {
+      gameOver();
+    } else if (userSequence.length == simonSequence.length) {
+      next();
     }
 }
 
-var intervalId = setInterval(simon, 1000);
+function gameOver() {
+    // hard reload the page.
+    location.reload(true);
 
-// simon makes selection, adds to sequence, then animates the sequence
-// game listens for user selection(s), compares vs simon sequence
+    confirm("Game over. Play again?");
+    if(confirm) {
+        start();
+    }
+}
 
+function stop() {
+    alert('User stopped the game');
+    location.reload(true);
+}
 
-document.getElementById('0').addEventListener('click', userClick, false);
-document.getElementById('1').addEventListener('click', userClick, false);
-document.getElementById('2').addEventListener('click', userClick, false);
-document.getElementById('3').addEventListener('click', userClick, false);
+function userClick() {
+    var userChoice = this.id;
+
+    lightUp(this);
+
+    userSequence.push(userChoice);
+
+    compareSequences();
+}
+
+function enableInput() {
+    document.getElementById('0').addEventListener('click', userClick, false);
+    document.getElementById('1').addEventListener('click', userClick, false);
+    document.getElementById('2').addEventListener('click', userClick, false);
+    document.getElementById('3').addEventListener('click', userClick, false);
+}
+
+function disableInput() {
+    document.getElementById('0').removeEventListener('click', userClick, false);
+    document.getElementById('1').removeEventListener('click', userClick, false);
+    document.getElementById('2').removeEventListener('click', userClick, false);
+    document.getElementById('3').removeEventListener('click', userClick, false);
+}
+
+document.getElementById('start').addEventListener('click', start, false);
+document.getElementById('stop').addEventListener('click', stop, false);
+
+// making squares disappear
+// timing of showing the squares/buttons
+// continuation of showing squares/buttons
+// where to put comparison of sequences
+// bug hunt some glitches
+// listening for user input
